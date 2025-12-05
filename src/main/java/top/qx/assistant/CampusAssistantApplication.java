86526021401e3326b902b283ab0e64f0b04bc971
Gwyndolin520 <2357232905@qq.com>
2025-12-05 08:15:33 +0800

    框架搭建

---
+++ src/main/java/top/qx/assistant/CampusAssistantApplication.java
@@ -0,0 +1,13 @@
+package top.qx.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusAssistantApplication.class, args);
+    }
+
+}
---
+++ src/main/java/top/qx/assistant/model/RequestDTO.java
@@ -0,0 +1,9 @@
+package top.qx.assistant.model;
+
+import lombok.Data;
+
+@Data
+public class RequestDTO {
+    private String userId;
+    private String message;
+}
